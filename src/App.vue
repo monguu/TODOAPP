6d26d714b2e48210fd3e44104918ffba023ceccb
mooonggu <bhnnjm1@naver.com>
2021-02-11 00:06:02 +0900

    todo app

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="App" class="container">
+    <h1 class="text-center">ToDo App</h1>
+    <input
+      v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+      ref="answer"
+    />
+    <div>
+      <TodoApp v-for="todo in todos" :key="todo.id" :todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoApp from "@/components/todo.vue";
+export default {
+  components: {
+    TodoApp,
+  },
+  data() {
+    return {
+      todoText: "",
+      todos: [
+        { id: 1, text: "play game", checked: false },
+        { id: 2, text: "hello coding", checked: false },
+      ],
+    };
+  },
+
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="checkbox" :checked="todo.checked" />
+    <span class="ml-3">{{ todo.text }}</span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
